9bfc978d74b7da6715b0e1764282cc1275dc7014
sun.ao <sunao2002002@gmail.com>
2017-01-13 15:38:49 +0800

    add configs

--- install.sh
+++ install.sh
@@ -3,5 +3,19 @@
 PWD=$(pwd)
 echo $PWD
 ln -s ${PWD} ~/.oh-my-zsh
+if [ -e ~/.zshrc ]; then
+    mv ~/.zshrc ~/.zshrc_bak
+fi
+
 ln -s ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
+if [ -e ~/.tmux.conf ]; then
+    mv ~/.tmux.conf ~/.tmux.conf_bak
+fi
+ln -s ~/.oh-my-zsh/templates/tmux.conf ~/.tmux.conf
+
+
+if [ -e ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc_bak
+fi
+ln -s ~/.oh-my-zsh/templates/vimrc ~/.vimrc
